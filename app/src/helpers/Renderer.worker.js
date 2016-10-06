9763c86ec01f4d9fb5439d1af0960f57fc745508
malbernaz <albernazmiguel@gmail.com>
2016-10-06 18:33:50 -0300

    updates

---
+++ app/src/helpers/Renderer.worker.js
@@ -0,0 +1,63 @@
+import marked, { Renderer } from 'marked'
+import { getLanguage, highlight } from './highlightjs'
+
+const renderer = new Renderer()
+
+marked.setOptions({ gfm: true, renderer })
+
+renderer.code = (code, language) => {
+  const validLang = !!(language && getLanguage(language))
+
+  const highlighted = validLang ?
+    highlight(language, code).value : code
+
+  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`
+}
+
+renderer.list = (body, ordered) => {
+  const items = body
+    .split(/<li>|<\/li>/)
+    .filter(s => s !== '')
+
+  let hasCheckbox = false
+  const output = items
+    .map(i => {
+      if (i.match(/\r?\n|\r/)) return i
+      if (i.match(/^\[x\]\s/) || i.match(/^\[\s\]\s/)) {
+        hasCheckbox = true
+        return i.match(/^\[x\]\s/) ?
+          ({ text: i.replace(/^\[x\]\s/, ''), checked: 'checked' }) :
+          ({ text: i.replace(/^\[\s\]\s/, ''), checked: '' })
+      }
+      return { text: i }
+    })
+    .map(i => {
+      if (typeof i !== 'object' && i.match(/\r?\n|\r/)) {
+        if (i.length > 1) return `<li>${i}</li>`
+        return i
+      }
+      return hasCheckbox ?
+        `<li style="list-style:none;">
+          <input type="checkbox" disabled ${i.checked}>
+            &nbsp;&nbsp;${i.text}
+          </input>
+        </li>` :
+        `<li>${i.text}</li>`
+    })
+
+  if (hasCheckbox) {
+    return ordered ?
+      `<ol style="margin-left:0;padding:0">${output.join('')}</ol>` :
+      `<ul style="margin-left:0;padding:0">${output.join('')}</ul>`
+  }
+
+  return ordered ?
+    `<ol>${output.join('')}</ol>` :
+    `<ul>${output.join('')}</ul>`
+}
+
+self.onmessage = e => {
+  e.preventDefault()
+
+  self.postMessage(marked(e.data.raw))
+}
